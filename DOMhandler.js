925bb1f8148acac927f2d171f40ce650918c2747
ellisthomas <ellisthomas80@gmail.com>
2017-03-25 09:18:14 -0500

    everything working

--- DOMhandler.js
+++ DOMhandler.js
@@ -1,4 +1,3 @@
-
 //////////////// EVENT LISTENERS ////////////////
 
 var name = document.getElementById("name");
@@ -14,28 +13,27 @@ cancel.addEventListener("click", clear);
 
 
 function printDonor() {
-var newDonor = {};
-	newDonor.name = document.getElementById("name").value;
-	newDonor.email = document.getElementById("email").value;
-	newDonor.amount = document.getElementById("amount").value;
-	newDonor.type = document.getElementsByName("amountType")
-	for (var i = 0; i < 2; i++) {
-		if (newDonor.type[i].checked) {
-			newDonor.type = newDonor.type[i].value
-		}
-	}
-	if (newDonor.name === "" || newDonor.email === "" || newDonor.amount === "") {
-		alert("Easy there killer, fill in the blanks!")
-	} else {
-		DonorInfo.addDonor(newDonor);
-		DonorInfo.showDonor(newDonor);
-		clear();
-	};
+    var newDonor = {};
+    newDonor.name = document.getElementById("name").value;
+    newDonor.email = document.getElementById("email").value;
+    newDonor.amount = document.getElementById("amount").value;
+    newDonor.type = document.getElementsByName("amountType")
+    for (var i = 0; i < 2; i++) {
+        if (newDonor.type[i].checked) {
+            newDonor.type = newDonor.type[i].value
+        }
+    }
+    if (newDonor.name === "" || newDonor.email === "" || newDonor.amount === "") {
+        alert("Easy there killer, fill in the blanks!")
+    } else {
+        DonorInfo.addDonor(newDonor);
+        DonorInfo.showDonor(newDonor);
+        clear();
+    };
 };
 
 function clear() {
-	name.value = "";
-	email.value = "";
-	amount.value = "";
+    name.value = "";
+    email.value = "";
+    amount.value = "";
 };
-
